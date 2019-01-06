3b72ea1e57ebd13c31dea9154c89d976785df76e
bartosz-paternoga <bpaternoga@hotmail.com>
2019-01-06 14:35:41 +0100

    Updates

---
+++ precache-manifest.f97a5e029c5d43e315d40f46206148d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c42817f2ecd29e79574e",
+    "url": "/webcam_classifier-tfjs/static/css/main.acdd894f.chunk.css"
+  },
+  {
+    "revision": "c42817f2ecd29e79574e",
+    "url": "/webcam_classifier-tfjs/static/js/main.c42817f2.chunk.js"
+  },
+  {
+    "revision": "7568c899a0344351fdf9",
+    "url": "/webcam_classifier-tfjs/static/js/1.7568c899.chunk.js"
+  },
+  {
+    "revision": "92a9f6886e1362e79fd1",
+    "url": "/webcam_classifier-tfjs/static/js/runtime~main.92a9f688.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webcam_classifier-tfjs/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9c432cd066f605e48e9acb0cb60fc6a9",
+    "url": "/webcam_classifier-tfjs/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/webcam_classifier-tfjs/precache-manifest.f97a5e029c5d43e315d40f46206148d6.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/webcam_classifier-tfjs/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
